a8b82906022cbb8352cb95c9eb756195dcb7e4eb
ARVIND SAI K <arvindsaik@users.noreply.github.com>
2018-04-08 04:25:41 +0530

    Simultaneous Equation back end support

--- Server/digit_reco_server/python_utils/sympySolver.py
+++ Server/digit_reco_server/python_utils/sympySolver.py
@@ -8,3 +8,12 @@ def solveIt(equation):
         return solution
     except:
         return solution
+    
+def solveSystem(equations):
+    parsed_equations = []
+    for equation in equations:
+        parsed_equations.append(parse_expr(equation))
+    result = {}
+    result["solution"] = solve(parsed_equations)
+    result["equation"] = parsed_equations
+    return result
